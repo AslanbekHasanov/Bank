93e626fb1a63f590e2a1d6056e3fa8467dce6224
User <User@WIN-8BH8LKBRQNO>
2023-12-15 22:09:12 +0500

    He gave an assessment of the account

---
+++ Universal_kassa/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Universal_kassa
+{
+         public abstract class Account
+         {
+            public string? AccountNumber { get; set; }
+            public decimal Balance { get; protected set; }
+
+            public abstract void Deposit(decimal amount);
+             
+         }
+
+}
+
---
+++ Universal_kassa/CheckingAccount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Universal_kassa;
+
+namespace Bosh_menyu
+{
+    public abstract class CheckingAccount: Account
+    {
+        public virtual bool Withdraw(decimal amount)
+        {
+            if (Balance >= amount)
+            {
+                Balance -= amount;
+                return true;
+            }
+            return false;
+        }
+        public decimal GetBalance() => Balance;
+    }
+}
