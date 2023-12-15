e2e5be70e11b51e96e3a0df0dc2b1cbd9cdab72a
User <User@WIN-8BH8LKBRQNO>
2023-12-15 22:23:52 +0500

    Client

---
+++ Universal_kassa/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Universal_kassa;
+
+namespace Bosh_menyu
+{
+    public class Client
+    {
+        public string Name { get; set; }
+        public Account Account { get; set; }
+
+        public Client(string name, Account account)
+        {
+            Name = name;
+            Account = account;
+        }
+
+        public string GetDetails() => $"Name: {Name}, Account number: {Account.AccountNumber}";
+    }
+}
