d5e6e6eebfe93dae3199ff45506ad61cc029be4f
User <User@WIN-8BH8LKBRQNO>
2023-12-21 14:32:59 +0500

    this is project BankApp

---
+++ Bank1/Program.cs
@@ -0,0 +1,14 @@
+﻿using Bosh_menyu;
+
+BankApp app = new BankApp();
+Client d = new Client();
+d.GetDetails();
+
+
+/*app.CreateClientAndAccount("John Doe", new CheckingAccount { AccountNumber = "12345678", Balance = 1000 });
+app.CreateClientAndAccount("Jane Doe", new CheckingAccount { AccountNumber = "98765432", Balance = 500 });
+*/
+app.Transfer("John Doe", "Jane Doe", 200);
+
+app.ShowBalance("John Doe");
+app.ShowBalance("Jane Doe");
---
+++ Universal_kassa/BankApp.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Universal_kassa;
+
+namespace Bosh_menyu
+{
+    public class BankApp
+    {
+        private BANK bank;
+
+        public BankApp()
+        {
+            bank = new BANK();
+        }
+
+        public void CreateClientAndAccount(string name, Account account)
+        {
+            bank.OpenAccount(name, account);
+        }
+
+        public bool Transfer(string senderName, string receiverName, decimal amount)
+        {
+            Client? sender = bank.GetClients().Find(c => c.Name == senderName);
+            Client? receiver = bank.GetClients().Find(c => c.Name == receiverName);
+            if (sender != null && receiver != null)
+                return bank.TransferMoney(sender, receiver, amount);
+            return false;
+        }
+
+        public void ShowBalance(string clientName)
+        {
+            Client? client = bank.GetClients().Find(c => c.Name == clientName);
+            if (client != null)
+                Console.WriteLine($"{client.Name}'s account balance: {client.Account.GetBalance():C2}");
+            else
+                Console.WriteLine("Client not found!");
+        }
+
+        public void CreateClientAndAccount(string v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
